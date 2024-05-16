a354387e4e9a94bbf5d73294a8a78fe69f99654e
Jimmy Brock <jimmybrock@gmail.com>
2024-05-16 15:36:08 -0400

    Add files via upload

---
+++ submission/query_4.sql
@@ -0,0 +1,64 @@
+-- Batch backfill query that can populate the entire 
+-- actors_history_scd table in a single query
+INSERT INTO  jimmybrock65656.actors_history_scd
+-- get previous year data so we can compare to current year
+WITH lagged AS (
+        SELECT
+            actor,
+            quality_class,
+            -- get last years quality class
+            LAG(quality_class, 1) OVER (
+                    PARTITION BY actor
+                    ORDER BY
+                        current_year
+                ) AS quality_class_last_year,
+            is_active,
+            -- get last years active status
+            LAG(is_active, 1) OVER (
+                    PARTITION BY actor
+                    ORDER BY
+                        current_year
+                )
+            as is_active_last_year,
+            current_year
+        FROM
+             jimmybrock65656.actors
+        WHERE
+            current_year <= 2021
+    ),
+    -- how long has actor been active
+    streaked AS (
+        SELECT
+            *,
+            SUM(
+                -- compare active status and quality_class for 
+                -- changes to see if streaked
+                CASE
+                    WHEN is_active <> is_active_last_year
+                    OR quality_class <> quality_class_last_year 
+                        THEN 1
+                    ELSE 0
+                END
+            ) OVER (
+                PARTITION BY actor
+                ORDER BY
+                    current_year
+            ) AS tracked_changes
+        FROM
+            lagged
+    )
+    -- new records for scd history
+SELECT
+    actor,
+    quality_class,
+    is_active,
+    MIN(current_year) AS start_date,
+    MAX(current_year) AS end_date,
+    2021 as current_year
+FROM
+    streaked
+GROUP BY
+    actor,
+    is_active,
+    quality_class,
+    tracked_changes
---
+++ submission/query_5.sql
@@ -0,0 +1,110 @@
+-- incremental query that can populate a single years 
+-- worth of the actors_history_scd table by combining 
+-- the previous year's SCD data with the new incoming data from the actors table for this year.
+insert into jimmybrock65656.actors_history_scd
+-- last year data for a single year
+WITH
+  last_year_scd AS (
+    SELECT
+      *
+    FROM
+      jimmybrock65656.actors_history_scd
+    WHERE
+      current_year = 1943
+  ),
+  -- this year data for a single year
+  current_year_scd AS (
+    SELECT
+      *
+    FROM
+      jimmybrock65656.actors
+    WHERE
+      current_year = 1944
+  ), 
+  -- combine two datasets 
+  combined as 
+    (
+    SELECT
+    -- get not null values if possible
+    coalesce(ls.actor, cs.actor) as actor,
+    coalesce(ls.start_date, cs.current_year) as start_date,
+    coalesce(ls.end_date, cs.current_year) as end_date,
+    -- check if data change between years for 
+    -- active status and quality_change
+    CASE
+      WHEN (ls.is_active <> cs.is_active) or 
+          (ls.quality_class <> cs.quality_class) THEN 1
+      WHEN (ls.is_active = cs.is_active) and 
+          (ls.quality_class = cs.quality_class) THEN 0
+    END AS did_change, 
+    ls.is_active as is_active_last_year, 
+    cs.is_active as is_active_this_year,
+    ls.quality_class as quality_class_last_year, 
+    cs.quality_class as quality_class_this_year,
+    1944 as current_year
+FROM
+  -- full outer join to get all change records
+  last_year_scd ls
+  FULL OUTER JOIN current_year_scd cs ON cs.actor = ls.actor
+  AND cs.current_year = ls.end_date + 1
+  ), changes as
+  (
+  select actor,current_year,
+    case when did_change = 0  
+        then ARRAY[cast(row(quality_class_last_year,
+                      is_active_last_year,
+                      start_date,
+                      end_date+1)
+                      as ROW(
+                            quality_class varchar,
+                            is_active boolean,
+                            start_date integer,
+                            end_date integer
+                  ))]
+          when did_change = 1
+          then ARRAY[cast(row(quality_class_last_year,
+                      is_active_last_year,
+                      start_date,
+                      end_date) as 
+                      ROW(
+                            quality_class varchar,
+                            is_active boolean,
+                            start_date integer,
+                            end_date integer
+                      )),
+                      cast(
+                      row(quality_class_this_year,
+                      is_active_this_year,
+                      current_year,
+                      current_year)
+                      as ROW(
+                            quality_class varchar,
+                            is_active boolean,
+                            start_date integer,
+                            end_date integer
+                      ))]
+        when did_change is null
+        then ARRAY[cast(row(
+        coalesce(quality_class_last_year,quality_class_this_year),
+                      coalesce(is_active_last_year,is_active_this_year),
+                      start_date,
+                      end_date)
+                      as ROW(
+                            quality_class varchar,
+                            is_active boolean,
+                            start_date integer,
+                            end_date integer
+                  ))]
+    end as change_array
+  from combined
+  )
+  -- unnest changes and join
+  select 
+  actor,
+  arr.quality_class,
+  arr.is_active,
+  arr.start_date,
+  arr.end_date,
+  current_year
+  from changes 
+  cross join unnest(change_array) as arr
